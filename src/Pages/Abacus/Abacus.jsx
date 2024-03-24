0f6067cc18bab7cb4e65ea6c44462b93be63ab0b
sama-kun <samgar.robot@gmail.com>
2024-03-24 11:39:35 +0500

    test

--- src/Pages/Abacus/Abacus.jsx
+++ src/Pages/Abacus/Abacus.jsx
@@ -4,26 +4,60 @@ import OrdersLeft from "../../Components/OrdersLeft/OrdersLeft";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import ru from "date-fns/locale/ru";
+import ModalEditOrder from "../../Modals/ModalEditOrder/ModalEditOrder";
 const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InNhbWEiLCJpZCI6NDMsInJvbGUiOiJ1c2VyIiwiaWF0IjoxNzExMjM2MTk2LCJleHAiOjE3MTEzMjI1OTZ9.hXjzpXeC3hFzC-Jg-qdmpizbdsIAIw9Gk7qsErKWw4M'
 
 const Abacus = () => {
   const { sidebarOpen, checkedClient } = useContext(UserContext);
   const [beforeSelectedDate, setSelectedDateBefore] = useState(new Date());
   const [afterSelectedDate, setSelectedDateAfter] = useState(new Date());
-  const [orders, setOrders] = useState([
-    {
-      id: 1,
-      createdAt: '2042-03-23',
-      amount: 1000
-    }
+  const [orders, setOrders] = useState([]);
+  const [client, setClient] = useState({});
+  const [paid, setPaid] = useState(0);
+  const [remains, setRemains] = useState(0);
+  const [selectedOrder, setSelectedOrder] = useState(null);
 
-  ]);
+  const handleEditClick = (order) => {
+  setSelectedOrder(order);
+  // Здесь можно также открыть модальное окно для редактирования
+};
 
   const fetchOrdersOfClients = async () => {
     try {
       // const queryParams = new URLSearchParams(params).toString();
       const response = await fetch(
-        `https://monkfish-app-v8pst.ondigitalocean.app/api/order/?relations[0]=owner&filter[owner][id]=${checkedClient}&sort[createdAt]=esc&dateFilter[startDate]=${}`,
+        `https://monkfish-app-v8pst.ondigitalocean.app/api/order/?relations[0]=owner&relations[1]=paymentHistories&filter[owner][id]=${checkedClient}`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+
+      const data = await response.json();
+      let sumPaid = 0;
+      let sumRemains = 0;
+      data.data.records.map((value, index) => {
+        sumRemains += Number(value.remains);
+        sumPaid += Number(value.amount - value.remains);
+      })
+      setPaid(sumPaid);
+      setRemains(sumRemains);
+      setOrders(data.data.records);
+    } catch (error) {
+      console.error("Error during fetch:", error);
+    }
+  };
+
+  const fetchUser = async () => {
+    try {
+      // const queryParams = new URLSearchParams(params).toString();
+      const response = await fetch(
+        `https://monkfish-app-v8pst.ondigitalocean.app/api/user/${checkedClient}?relations[0]=ordersAsClient`,
         {
           headers: {
             Authorization: `Bearer ${token}`,
@@ -38,7 +72,16 @@ const Abacus = () => {
       const data = await response.json();
       // Обработка полученных данных
       console.log(data)
-      setOrders(data.data.records);
+      const sizeOrders = data.data.ordersAsClient?.length;
+      let summa = 0;
+      data.data.ordersAsClient?.map((value, index) => {
+        console.log(value.amount)
+        summa += Number(value.amount)
+      })
+      console.log(summa)
+      data.data.sizeOrders = sizeOrders;
+      data.data.summa = summa;
+      setClient(data.data);
     } catch (error) {
       console.error("Error during fetch:", error);
     }
@@ -71,7 +114,8 @@ const Abacus = () => {
   };
   console.log(checkedClient)
   useEffect(() => {
-    fetchOrdersOfClients(checkedClient);
+    fetchUser()
+    fetchOrdersOfClients();
   }, [checkedClient]);
   return (
     <div className={`abacus-page ${sidebarOpen ? "p-o" : "p-c"}`}>
@@ -81,19 +125,19 @@ const Abacus = () => {
         <div className="right-abacus">
           <div className="abacus-info">
             <span>Клиент:</span>
-            <span>{"{ Имя клиента}"}</span>
+            <span>{client.username}</span>
           </div>
           <div className="abacus-info">
             <span>Телефон:</span>
-            <span>{"{+7 321 323 42 42}"}</span>
+            <span>{client.phone}</span>
           </div>
           <div className="abacus-info">
             <span>Сумма заказов:</span>
-            <span>{"{сумма}"}</span>
+            <span>{client.summa}</span>
           </div>
           <div className="abacus-info">
             <span>Кол-во заказов:</span>
-            <span>{"{кол-во}"}</span>
+            <span>{client.sizeOrders}</span>
           </div>
           <div className="date-pick">
             От
@@ -119,9 +163,12 @@ const Abacus = () => {
             <th>№ Накладного</th>
             <th>Дата</th>
             <th>Сумма</th>
+            <th>Оплачен</th>
+            <th>Долги</th>
+            <th></th>
           </tr>
         </thead>
-        <tbody>
+        <tbody className="text-gray-700">
           {orders.length != 0 ? (
             orders.map((order) => {
               return (
@@ -129,8 +176,16 @@ const Abacus = () => {
                   <td>{order.id}</td>
                   <td>{formatDate(new Date(order.createdAt))}</td>
                   <td>{formatToRubles(order.amount)}</td>
-                  <td></td>
-                  <td></td>
+                  <td>{ formatToRubles(order.amount - order.remains) }</td>
+                  <td>{ formatToRubles(order.remains) }</td>
+                  <td>
+                    <button
+                      className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded"
+                      onClick={() => handleEditClick(order)}
+                    >
+                Редактировать
+            </button>
+                  </td>
                 </tr>
               );
             })
@@ -138,11 +193,26 @@ const Abacus = () => {
             <tr>
               <td>Заказов к сожалению нету</td>
             </tr>
-          )}
+              )}
+              <tr className="bg-gray-200">
+                  <td><strong>Итог:</strong></td>
+                  <td></td>
+                  <td></td>
+                  <td>{ formatToRubles(paid) }</td>
+                  <td>{ formatToRubles(remains) }</td>
+                  <td></td>
+                </tr>
         </tbody>
       </table>
         </div>
       </div>
+      {selectedOrder && (
+        <ModalEditOrder
+          order={selectedOrder}
+          onClose={() => setSelectedOrder(null)}
+          // Другие пропсы, которые могут понадобиться для редактирования
+        />
+      )}
     </div>
   );
 };
