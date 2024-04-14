d4b8ba488f93ffe35386c09bd77aa1c1e55ed50c
santonssss <sarvarkalmuratov370@gmail.com>
2024-04-15 04:10:52 +0500

    test

--- src/Pages/MobilePage/MobilePage.jsx
+++ src/Pages/MobilePage/MobilePage.jsx
@@ -4,6 +4,9 @@ import ChartWrapper from "../../Components/Chart/Chart";
 import MobileDelivery from "../../Components/MobileDelivery/MobileDelivery";
 
 const MobilePage = () => {
+  useEffect(() => {
+    console.log("root");
+  }, []);
   return (
     <div className="mobile-page">
       <ChartWrapper />
